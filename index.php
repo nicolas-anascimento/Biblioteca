6675c213a1179b05eb3559820a76aa04607606d0
Nicolas Alves Nascimento <74421367+nicolas-anascimento@users.noreply.github.com>
2025-09-15 21:07:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="conteudo">
+            <h1>Pesquisar</h1><br>
+            <form>
+                <label for="nome">Nome:</label><br>
+                <input type="text" id="nome" name="nome"><br><br>
+                <input type="button" value="Pesquisar">
+                <input type="button" value="Limpar">
+            </form>
+
+        </div>
+    </div>
+
+<?php
+    /*
+    fazer um código para que procure o manhwa ou mangá e coloque os dados dele ai, caso queria atualizar algo peça para inserir os novos dados (cap e data ou atualizar o hiato)
+    
+    */
+?>
+</body>
+</html>
